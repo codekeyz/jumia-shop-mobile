f0e9ad5fd86ae45a75d0c0448db07b45a45eb37e
Chima Precious <ositachima.co@gmail.com>
2022-07-14 20:38:01 +0000

    register & initialize graphql client

---
+++ lib/server/services/injector.dart
@@ -0,0 +1,12 @@
+import 'package:get_it/get_it.dart';
+import 'package:gql_client/gql_client.dart';
+
+final getIt = GetIt.instance;
+
+Future<void> registerServices() async {
+  const gqlConfig = GraphqlLiteConfig(
+    apiEndpoint: "http://localhost:3000/shop-api",
+  );
+
+  getIt.registerSingleton<GraphQLClient>(GraphQLClient.create(gqlConfig));
+}
