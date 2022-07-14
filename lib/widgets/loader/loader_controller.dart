4d75a3409efda28ec1f9814544b8ec4ea16430bf
Chima Precious <ositachima.co@gmail.com>
2022-07-14 16:49:24 +0000

    Initial commit

---
+++ lib/widgets/loader/loader_controller.dart
@@ -0,0 +1,26 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/utils/data_stream_mixin.dart';
+import 'package:flutter_boilerplate/widgets/loader/loader_screen.dart';
+import 'package:provider/provider.dart';
+
+class LoaderController extends ChangeNotifier with DataStreamMixin<LoaderData> {
+  AnimationController? _controller;
+
+  static LoaderController of(BuildContext context) {
+    return Provider.of<LoaderController>(context, listen: false);
+  }
+
+  void withController(AnimationController animCtrl) {
+    _controller = animCtrl;
+  }
+
+  void showLoader({String? message}) {
+    _controller?.forward();
+    addEvent(LoaderData(state: LoaderState.showing, message: message));
+  }
+
+  void hideLoader() {
+    _controller?.reverse();
+    addEvent(const LoaderData(state: LoaderState.hidden));
+  }
+}
---
+++ lib/widgets/loader/loader_screen.dart
@@ -0,0 +1,66 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_boilerplate/widgets/loader/loader_controller.dart';
+import 'package:flutter_boilerplate/widgets/loader/loader_view.dart';
+
+enum LoaderState { showing, hidden }
+
+class LoaderData {
+  final LoaderState state;
+  final String? message;
+
+  const LoaderData({
+    required this.state,
+    this.message,
+  });
+}
+
+class LoaderScreen extends StatefulWidget {
+  final Widget child;
+
+  const LoaderScreen({Key? key, required this.child}) : super(key: key);
+
+  @override
+  State<LoaderScreen> createState() => _LoaderScreenState();
+}
+
+class _LoaderScreenState extends State<LoaderScreen> with SingleTickerProviderStateMixin {
+  late AnimationController animationCtrl;
+
+  @override
+  void initState() {
+    super.initState();
+    animationCtrl = AnimationController(
+      vsync: this,
+      duration: const Duration(milliseconds: 120),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    final loaderCtrl = LoaderController.of(context)..withController(animationCtrl);
+    return StreamBuilder<LoaderData>(
+      builder: (_, snap) {
+        final data = snap.data;
+        final _loading = data?.state == LoaderState.showing;
+        return Stack(
+          children: [
+            widget.child,
+            if (_loading)
+              LoaderView(
+                controller: animationCtrl,
+                message: data?.message,
+              ),
+          ],
+        );
+      },
+      stream: loaderCtrl.stream,
+      initialData: loaderCtrl.lastEvent,
+    );
+  }
+
+  @override
+  void dispose() {
+    animationCtrl.dispose();
+    super.dispose();
+  }
+}
